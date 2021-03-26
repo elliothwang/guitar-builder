9fcd8c46aa718701dee4b1373fa1ca51ae2a3f7c
Elliot Hwang <elliothwang@MacBook-Pro-2.local>
2021-03-26 16:23:47 -0700

    step 13

--- controllers/guitars.js
+++ controllers/guitars.js
@@ -2,9 +2,10 @@ const Guitar = require('../models/guitar');
 
 module.exports = {
   index
-}
+};
 
 function index (req, res) {
   res.send("home page");
   // res.render('index', {title : "Home"})
-}
+};
+
