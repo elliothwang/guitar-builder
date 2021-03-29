2e975a74c12be53251a278b0e5b6669c82a00873
Elliot Hwang <elliothwang@MacBook-Pro-2.local>
2021-03-29 10:53:41 -0700

    body schema

--- models/guitar.js
+++ models/guitar.js
@@ -2,8 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // embedded schemas (neck & body)
+const bodySchema = new Schema({
+  model : {
+    type : String,
+    enum : ['Dreadnought', 'Auditorium', 'Parlour', 'Classical'],
+    default: 'Dreadnought',
+  },
+  topWood : {
+    type: String,
+    enum : ['Spruce', 'Cedar', 'Mahogany', 'Maple'],
+    default : 'Cedar'
+  },
+  sideWood : {
+    type: String,
+    enum : ['Spruce', 'Mahogany', 'Maple', 'Koa', 'Rosewood', 'Walnut'],
+    default : 'Mahogany'
+  },
+  backWood : {
+    type: String,
+    enum : ['Spruce', 'Cedar', 'Mahogany', 'Maple'],
+    default : 'Mahogany'
+  }
+});
 
 // parent schema guitarSchema
+const guitarSchema = new Schema({
+  name : String,
+  body : [bodySchema],
+  neck : [neckSchema]
+})
 
   // neck : [neckSchema]
   // body : [bodySchema]
