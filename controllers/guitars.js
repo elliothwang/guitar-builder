d1f21fa948ca2217a22ab61308e991a9f12a10f4
Elliot Hwang <elliothwang@MacBook-Pro-2.local>
2021-03-29 09:05:26 -0700

    make routes

--- controllers/guitars.js
+++ controllers/guitars.js
@@ -4,10 +4,24 @@ const Guitar = require('../models/guitar');
 // req.body.user = req.user._id
 
 module.exports = {
-  index
+  index,
+  new: newGuitar,
+  show,
+  create
 };
 
 function index (req, res) {
-  res.send("home page");
-  // res.render('index', {title : "Home"})
+  res.render('guitars/index', {title : "Guitar Home"})
+};
+
+function newGuitar (req, res) {
+  res.render('guitars/new', {title : "Create New Custom"})
+};
+
+function show (req, res) {
+  res.render('guitars/show', {title : "Show All Customs"})
+}
+
+function create (req, res) {
+  res.render(`/guitars/${guitar._id}`, {title : "Create Guitar in DB"})
 };
--- routes/guitars.js
+++ routes/guitars.js
@@ -3,6 +3,20 @@ var router = express.Router();
 var guitarCtrl = ('../controllers/guitar');
 
 /* GET users listing. */
-// router.get();
+// router.get('/', guitarCtrl.index);
+router.get('/', function (req, res) {
+  res.send("guitars home page");
+});
+// router.get('/new', isLoggedIn, moviesCtrl.new);
+router.get('/new', function (req, res) {
+  res.send("make a custom guitar page");
+});
+
+// router.get('/:id', isLoggedIn, moviesCtrl.show);
+// router.post('/', isLoggedIn, moviesCtrl.create);
+
+router.get('/saved', function (req, res) {
+  res.send('See your saved customs page');
+});
 
 module.exports = router;
