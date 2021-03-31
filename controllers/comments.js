c2222ef0534b92c69754e429db826686895f1898
Elliot Hwang <elliothwang@MacBook-Pro-2.local>
2021-03-31 09:46:02 -0700

    create delete comment func

--- controllers/comments.js
+++ controllers/comments.js
@@ -12,12 +12,19 @@ function create (req, res) {
     req.body.userAvatar = req.user.avatar;
 
     guitar.comments.push(req.body);
-    guitar.save(function(err) {
-      res.redirect(`/guitars/${guitar._id}`)
+    guitar.save(function (err) {
+      res.redirect(`/guitars/${guitar._id}`);
     });
   });
 };
 
 function deleteComment (req, res) {
-  
+  Guitar.findOne({'reviews._id': req.params.id}).then(function(guitar) {
+    const comment = guitar.comments.id(req.params.id);
+    if (!guitar.user.equals(req.user._id)) return res.redirect(`/guitars/${guitar._id}`);
+    comment.remove();
+    guitar.save(function (err) {
+      res.redirect(`/guitars/${guitar._id}`);
+    });
+  });
 };
